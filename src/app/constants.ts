6e42ed0bd3e3fbb6ab618e50474c1fc906f5bba2
Fabian-Obando <obandofabian03@gmail.com>
2026-07-01 19:16:40 -0600

    Agregar modelos, servicio, pipe y constantes

---
+++ src/app/constants.ts
@@ -0,0 +1,8 @@
+export const CODE_LEAGUES = [
+  "4335", // Spanish La Liga
+  "4328", // English Premier League
+  "4332", // German Bundesliga
+  "4331", // Italian Serie A
+  "4334", // French Ligue 1
+  "4337"  // Dutch Eredivisie
+];
---
+++ src/app/models/league.model.ts
@@ -0,0 +1,11 @@
+export interface ILeague {
+
+  idLeague: string;
+
+  strLeague: string;
+
+  strSport: string;
+
+  strLeagueAlternate: string;
+
+}
